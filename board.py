97b9ed7ade2a0f69c814f8139fb7366410880c42
David Link <dvlink@gmail.com>
2019-01-19 06:19:37 +0000

    implementing piece movement

--- board.py
+++ board.py
@@ -12,25 +12,31 @@ class Board(object):
            Create the pieces and add them to the board
         '''
         self.matrix= [[None for j in range(0, 8)] for i in range(0, 8)]
+        self.pieces = []
         self.setup()
 
     def setup(self, setup_data=STANDARD):
-        '''Set up the pieces on the board
+        '''Set up the pieces on the board based in input file
         '''
         for row in open(setup_data).readlines():
             row = row.strip()
-            p, pos = row.split('-')
-            piece = Pieces.getPiece(p)
-            self.placePiece(piece, pos)
 
-    def placePiece(self, piece, pos):
-        '''Place a piece on the board
+            # eq.: k-e1
+            char, coord = row.split('-')
+            piece = Pieces.getPiece(char)
+            position = Position(coord)
+
+            self.placePiece(piece, position)
+            self.pieces.append(piece)
+
+    def placePiece(self, piece, position):
+        '''Place a piece at a position on the board
              piece is a Piece Object
-             pos is in the form of a1, a2, etc
+             position is a Position Object
         '''
-        file = ord(pos[0])-97  # convert a,b,c, ... -> 0,1,2, ...
-        row  = int(pos[1])-1   # convert 1,2,3, ... -> 0,1,2, ...
-        self.matrix[row][file] = piece
+
+        self.matrix[position.y][position.x] = piece
+        piece.position = position
 
     def display(self):
         '''Generate and return an ascii representation of the board
@@ -52,5 +58,50 @@ class Board(object):
                 o += '    a   b   c   d   e   f   g   h\n'
         return o
 
+class Position(object):
+    '''Preside over squares on the board'''
+
+    @staticmethod
+    def xy2coord(x, y):
+        file_ = y+1
+        row = chr(x+97)
+        return '%s%s' % (row, file_)
+
+    def __init__(self, coord):
+        self.coord = coord
+
+    def __repr__(self):
+        return self.coord
+
+    @property
+    def x(self):
+        '''convert a,b,c, ... -> 0,1,2, ...'''
+        return ord(self.coord[0])-97
+
+    @property
+    def y(self):
+         '''convert 1,2,3, ... -> 0,1,2, ...'''
+         return int(self.coord[1])-1
+
+    def move(self, color, movement):
+        # eq: f1
+        direction, dist = movement
+        dist = int(dist)
+        vector = 1 if color == 'w' else -1
+        if direction == 'f':
+            y2 = self.y + (vector * dist)
+        else:
+            raise PositionError('unkonwn movment direction: %s' % direction)
+        coord = Position.xy2coord(self.x, y2)
+        return coord
+
+
 if __name__ == '__main__':
-    print Board().display()
+    board = Board()
+    print board.display()
+
+    print 'piece, pos., move forward 1:'
+    for i in range(0, 32):
+        p = board.pieces[i]
+        print p, p.position, p.possibleMoves()
+
--- pieces.py
+++ pieces.py
@@ -39,31 +39,50 @@ class Piece(object):
         if color not in ('w', 'b'):
             raise PiecesError('Unrecognized color: %s' % color)
         self.color = color
+        self.postion = None
 
     def __repr__(self):
         return self.char if self.color == 'w' else self.char.upper()
 
+    def possibleMoves(self):
+        'test case: move forward one'
+        c = self.position.move(self.color, 'f1')
+        return c
+
 class Pawn(Piece):
     char = 'p'
     value = 1
+    moves = ['f1', 'f2']
+    # to do: restrict f2 movements to rows 2 and 7
+    # to do: build custom capture logic
 
 class Knight(Piece):
     char = 'n'
     value = 3
+    moves = ['f2,l1', 'f2,r1',
+             'l2,f1', 'l2,b1',
+             'b2,l1', 'b2,r1',
+             'r2,f1', 'r2,b1']
 
 class Bishop(Piece):
     char = 'b'
     value = 3
+    moves = ['d*', 'e*', 'g*', 'h*']
 
 class Rook(Piece):
     char = 'r'
     value = 5
+    moves =['f*', 'b*', 'l*', 'r*']
 
 class Queen(Piece):
     char = 'q'
     value = 9
+    moves = ['f*', 'b*', 'l*', 'r*',
+             'd*', 'e*', 'g*', 'h*']
 
 class King(Piece):
     char = 'k'
     value = None
+    moves = ['f1', 'b1', 'l1', 'r1',
+             'd1', 'e1', 'g1', 'h1']
 
